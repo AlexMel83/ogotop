75bdd90496ce2f778168933ff4233f7a619a919b
AlexMel83 <meleshko.a.n@ukr.net>
2023-04-02 23:43:06 +0300

    addtocart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) =>{
+  let sum = 0;
+  props.orders.forEach(el=>sum+=Number.parseFloat(el.price))
+  return(<div>
+    {props.orders.map(el=>(
+      <Order onDelete={props.onDelete} key={el.id} item={el} />
+    ))}
+    <p className='summa'>Sum: {new Intl.NumberFormat().format(sum)}$ </p>
+  </div>)
+}
+const showNothing = () => {
+  return (<div className='empty'>
+    <h2>No goods</h2>
+  </div>)
+}
+
+export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
@@ -14,7 +31,9 @@ export default function Header() {
             </ul>
             <FaShoppingCart onClick={()=>setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
             {cartOpen && (
-              <div className='shop-cart' ></div>
+              <div className='shop-cart' >
+                {props.orders.length > 0 ? showOrders(props) : showNothing() }
+              </div>
             )}
         </div>
         <div className='presentation'></div>
